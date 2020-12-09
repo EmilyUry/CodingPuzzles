926e634e71c34d713f5adab354d985185acfd9e0
Emily Ury <ury.emily@gmail.com>
2020-12-09 12:10:08 -0500

    day 3 part 1 started, not solved

---
+++ Day3.py
@@ -0,0 +1,26 @@
+file = open('input2.txt')
+input = file.readlines()
+print(input[0])
+
+aline = input[0].strip()
+fullline = aline * 323 * 3
+print(len(fullline))
+position = fullline[0]
+print(position)
+if position == 'X':
+    print('true')
+else:
+    print('false')
+
+
+def makemap(line):
+    aline = line.strip()
+    fullline = aline * 323 * 3
+    return fullline
+
+
+for each in input:
+    makemap(each)
+
+# print(line1x)
+print(len(fulline[0]))
